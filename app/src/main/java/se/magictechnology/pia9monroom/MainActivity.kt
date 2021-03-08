278d3a66ad77f103c39ade46c2223007592aa480
Bill Mårtensson <bill@magictechnology.se>
2021-03-08 13:12:07 +0100

    Room

--- app/src/main/java/se/magictechnology/pia9monroom/MainActivity.kt
+++ app/src/main/java/se/magictechnology/pia9monroom/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
 import androidx.room.*
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.MainScope
@@ -15,28 +17,43 @@ class MainActivity : AppCompatActivity(), CoroutineScope by MainScope() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val MIGRATION_2_3 = object : Migration(2, 3) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("ALTER TABLE product ADD COLUMN catid INTEGER")
+            }
+        }
+
         val db = Room.databaseBuilder(
                 applicationContext,
                 AppDatabase::class.java, "pia9product"
-        ).build()
+        ).addMigrations(MIGRATION_2_3).build()
 
-        var testprodukt = Product(productName = "Äpple", productPrice = 78)
 
-        var testkategori = Category(categoryName = "Frukter")
+        var testkategori = Category(categoryName = "Möbler")
 
         launch(Dispatchers.IO) {
-            //db.productDao().insertAll(testprodukt)
+            /*
+            var testprodukt1 = Product(productName = "Äpple", productPrice = 78, catid = 1)
+            db.productDao().insertAll(testprodukt1)
+            var testprodukt2 = Product(productName = "Stol", productPrice = 98, catid = 2)
+            db.productDao().insertAll(testprodukt2)
+            var testprodukt3 = Product(productName = "Banan", productPrice = 5, catid = 1)
+            db.productDao().insertAll(testprodukt3)
+            var testprodukt4 = Product(productName = "Hylla", productPrice = 73500, catid = 2)
+            db.productDao().insertAll(testprodukt4)
+            */
+            //db.categoryDao().insertAll(testkategori)
 
-            db.categoryDao().insertAll(testkategori)
-
-            var alltheproducts = db.productDao().getExpensive(5)
+            var alltheproducts = db.productDao().getCat(2)
 
             var productsText = ""
             for(prod in alltheproducts)
             {
                 Log.d("pia9debug", prod.productName!! + " " + prod.productPrice.toString())
+                Log.d("pia9debug", prod.getPrettyInfo())
+                //productsText = productsText + prod.productName!! + " " + prod.productPrice.toString() + "\n"
 
-                productsText = productsText + prod.productName!! + " " + prod.productPrice.toString() + "\n"
+                productsText = productsText + prod.getPrettyInfo()
 
             }
 
@@ -55,7 +72,13 @@ data class Product(
         @ColumnInfo(name = "product_name") val productName: String?,
         @ColumnInfo(name = "product_price") val productPrice: Int?,
         val catid : Int?
-)
+) {
+
+    fun getPrettyInfo() : String
+    {
+        return productName!! + " " + productPrice.toString()
+    }
+}
 
 @Dao
 interface ProductDao {
@@ -65,7 +88,7 @@ interface ProductDao {
     @Query("SELECT * FROM product WHERE product_price > :price")
     fun getExpensive(price : Int): List<Product>
 
-    @Query("SELECT * FROM product WHERE catid > :cid")
+    @Query("SELECT * FROM product WHERE catid = :cid")
     fun getCat(cid : Int): List<Product>
 
 
@@ -94,7 +117,7 @@ interface CategoryDao {
     fun delete(category: Category)
 }
 
-@Database(entities = arrayOf(Product::class,Category::class), version = 2)
+@Database(entities = arrayOf(Product::class,Category::class), version = 3)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun productDao(): ProductDao
     abstract fun categoryDao(): CategoryDao
